5902ca2c8bb2cdc5c6919ffa907697c3c08052da
JayChenFE <chenjiehwy@163.com>
2018-09-19 22:19:03 +0800

    修改main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import filters from '@/util/filter'
+import {
+    Button,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    Message,
+    MessageBox,
+} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+for (const key in filters) {
+    Vue.filter(key, filters[key])
+}
+
+Vue.use(Button)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$prompt = MessageBox.prompt
+
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app')
---
+++ src/util/filter.ts
@@ -0,0 +1,33 @@
+interface Ifilters {
+    [friendlyDate: string]: (dateStr: string | Date) => string
+}
+
+
+const filters: Ifilters = {
+    friendlyDate(dateStr: string | Date): string {
+        const dateObj = typeof dateStr === 'object' ? dateStr : new Date(dateStr)
+        const time = dateObj.getTime()
+        const now = Date.now()
+        const span = now - time
+
+        let str = ''
+
+        switch (true) {
+            case span < 60000:
+                str = '刚刚'
+                break
+            case span < 1000 * 3600:
+                str = Math.floor(span / 60000) + '分钟前'
+                break
+            case span < 1000 * 3600 * 24:
+                str = Math.floor(span / (1000 * 3600)) + '小时前'
+                break
+            default:
+                str = Math.floor(span / (1000 * 3600 * 24)) + '天前'
+                break
+        }
+        return str
+    },
+}
+
+export default filters
